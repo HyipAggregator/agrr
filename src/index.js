d4376844ecc3009b2d218c85b5eeb05748168520
afisx <moiseenko.alexej@gmail.com>
2016-07-19 22:41:15 +0300

    Introduce MonHyipParser

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 _ = require('lodash');
 Promise = require('promise');
+request = require('request');
+cheerio = require('cheerio');
+
 
 var engine = require('./parsers/engine.js');
 
--- src/parsers/monhyip.js
+++ src/parsers/monhyip.js
@@ -1,16 +1,52 @@
+var util  = require('util');
 module.exports = {
-    parse : function (params) {
-     /*   request('http://monhyip.net/', function(error, response, body){
-                console.log('the decoded data is: ' + body)
+    parse: function (params) {
+        request('http://monhyip.net/', function (error, response, body) {
+                if (!error && response.statusCode == 200) {
+
+                    var $ = cheerio.load(body);
+
+                    var result = [];
+                    var blocks = $('.hyip_list_item');
+
+                    blocks.each(function(index, block){
+
+                        var hyipResult = {};
+
+                            hyipResult.id = index;
+                            hyipResult.labels = [];
+
+
+                        var elemnts = $('tr', block);
+
+                            elemnts.each(function(i, val){
+
+                                var lab = $('.label', val);
+
+                                lab.each(function(ind, value){
+
+                                    hyipResult.labels.push($(value));
+
+                                });
+
+                            });
+
+                        result.push(hyipResult);
+
+                    });
+
+                    var all = [];
+
+                    for (var i= 0; i < result.length; i++){
+
+                            result[i].name = $(result[i].labels[0]).children().text();
+                            result[i].url = $(result[i].labels[0]).children().html();
+
+                    }
+                    console.log(result);
+
+                }
             }
         );
-        var promise = new Promise();
-        var brokenHtml = '<div><span class="a">TEST</span>';
-
-        var $ = cheerio.load(brokenHtml);
-
-        console.log($('.a').html());
-        */
-        console.log('MONHYIP ::: ' + params);
     }
 }
