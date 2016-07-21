a4597c1f7caa7444aba046b2ce0dd35890a13a8f
afisx <moiseenko.alexej@gmail.com>
2016-07-21 23:24:02 +0300

    Introduce MonHyipParser (#7)
    Rebuild MonHyipParser

--- src/parsers/monhyip.js
+++ src/parsers/monhyip.js
@@ -1,16 +1,42 @@
 module.exports = {
-    parse : function (params) {
-     /*   request('http://monhyip.net/', function(error, response, body){
-                console.log('the decoded data is: ' + body)
+    parse: function (params) {
+        request('http://monhyip.net/', function (error, response, body){
+                if (!error && response.statusCode == 200) {
+
+                    var $ = cheerio.load(body);
+
+                    var result = [];
+
+                    var blocks = $('.hyip_list_item');
+
+                    blocks.each(function(index, block){
+
+                        var hyipResult = {};
+
+                            hyipResult.id = index;
+
+                            var elements = $('tr', block).slice(2, 3);
+                            var links_details = $('a', $(elements));
+
+                                    request('http://monhyip.net'+$(links_details).attr('href'), function (error, response, body){
+                                        if (!error && response.statusCode == 200) {
+                                                var $ = cheerio.load(body);
+                                                var project_page = $('.projdetails').first();
+                                                var lines = $('tr', $(project_page));
+                                                var link = $(lines[0]).find('a');
+                                                hyipResult.name = link.html();
+                                                    request('http://monhyip.net'+link.attr('href'), function (error, response){
+                                                        if (!error && response.statusCode == 200) {
+                                                            hyipResult.URL = response.client._host;
+                                                            console.log(hyipResult);
+                                                        }
+                                                    });
+                                        }
+                                    });
+                    });
+                    console.log(result);
+                }
             }
         );
-        var promise = new Promise();
-        var brokenHtml = '<div><span class="a">TEST</span>';
-
-        var $ = cheerio.load(brokenHtml);
-
-        console.log($('.a').html());
-        */
-        console.log('MONHYIP ::: ' + params);
     }
-}
+};
