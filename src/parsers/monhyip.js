7b42ea6e2add00ab19d940bcedfd6b9718b9b151
afisx <moiseenko.alexej@gmail.com>
2016-07-21 23:08:25 +0300

    Rebuild MonHyipParser

--- src/parsers/monhyip.js
+++ src/parsers/monhyip.js
@@ -1,12 +1,12 @@
-var util  = require('util');
 module.exports = {
     parse: function (params) {
-        request('http://monhyip.net/', function (error, response, body) {
+        request('http://monhyip.net/', function (error, response, body){
                 if (!error && response.statusCode == 200) {
 
                     var $ = cheerio.load(body);
 
                     var result = [];
+
                     var blocks = $('.hyip_list_item');
 
                     blocks.each(function(index, block){
@@ -14,39 +14,29 @@ module.exports = {
                         var hyipResult = {};
 
                             hyipResult.id = index;
-                            hyipResult.labels = [];
 
+                            var elements = $('tr', block).slice(2, 3);
+                            var links_details = $('a', $(elements));
 
-                        var elemnts = $('tr', block);
-
-                            elemnts.each(function(i, val){
-
-                                var lab = $('.label', val);
-
-                                lab.each(function(ind, value){
-
-                                    hyipResult.labels.push($(value));
-
-                                });
-
-                            });
-
-                        result.push(hyipResult);
-
+                                    request('http://monhyip.net'+$(links_details).attr('href'), function (error, response, body){
+                                        if (!error && response.statusCode == 200) {
+                                                var $ = cheerio.load(body);
+                                                var project_page = $('.projdetails').first();
+                                                var lines = $('tr', $(project_page));
+                                                var link = $(lines[0]).find('a');
+                                                hyipResult.name = link.html();
+                                                    request('http://monhyip.net'+link.attr('href'), function (error, response){
+                                                        if (!error && response.statusCode == 200) {
+                                                            hyipResult.URL = response.client._host;
+                                                            console.log(hyipResult);
+                                                        }
+                                                    });
+                                        }
+                                    });
                     });
-
-                    var all = [];
-
-                    for (var i= 0; i < result.length; i++){
-
-                            result[i].name = $(result[i].labels[0]).children().text();
-                            result[i].url = $(result[i].labels[0]).children().html();
-
-                    }
                     console.log(result);
-
                 }
             }
         );
     }
-}
+};
