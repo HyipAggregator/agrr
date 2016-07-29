1884d37d05ae0216c1b777d16a2ee586e2f03f80
afisx <moiseenko.alexej@gmail.com>
2016-07-29 14:40:53 +0300

    add invest-tracing-parser

--- src/parsers/moneyNewsOnline.js
+++ src/parsers/moneyNewsOnline.js
@@ -5,7 +5,6 @@ module.exports = {
 
                     var $ = cheerio.load(body);
 
-
                     var blocks = $('.list');
 
                     blocks.each(function(index, block){
@@ -21,21 +20,31 @@ module.exports = {
                                 var $ = cheerio.load(body);
                                 var project = $('.Pname').find('a');
                                 var plans = $('.plans').find('b');
+                                var status = $('.ul_1').find('tr').first().find('img');
                                 var banner = $('.td_list').find('#ld').prev().find('img');
                                 hyipResult.name = project.html();
                                 hyipResult.plans = plans.text();
+                                hyipResult.status = status.attr('src');
                                 hyipResult.banner_code = banner.attr('src');
-                                request('http://money-news-online.com/monitoring/mno/'+project.attr('href'), {gzip:true}, function (error, response, body){
+                                request('http://money-news-online.com/monitoring/mno/'+project.attr('href'), function (error, response, body){
                                     if (!error && response.statusCode == 200){
                                         hyipResult.URL = '';
                                         /*params.save(hyipResult);
                                         console.log(hyipResult);*/
-                                    }else{console.log(error)}
+                                    }else{
+                                        /*console.log('ERROR from last response '+error);
+                                        console.log(hyipResult.id);*/
+                                    }
                                 });
 
+                            }else{
+                                /*console.log('ERROR from second response '+error);
+                                console.log(hyipResult.id);*/
                             }
                         });
                     });
+                }else{
+                    /*console.log('ERROR from first response '+error)*/
                 }
             }
         );
--- src/parsers/monhyip.js
+++ src/parsers/monhyip.js
@@ -14,32 +14,41 @@ module.exports = {
                         hyipResult.id = index;
 
                         var elements = $('tr', block).slice(2, 3);
-                        var link_details = $('a', $(elements));
+                        var link_details = $('a', $(elements)).attr('href');
 
-                            request('http://monhyip.net'+link_details.attr('href'), function (error, response, body){
+                            request({uri: 'http://monhyip.net'+link_details, host: 'monhyip.net', path: link_details}, function (error, response, body){
                                 if (!error && response.statusCode == 200) {
                                     var $ = cheerio.load(body);
                                     var project_page = $('.projdetails').first();
                                     var lines = $('tr', $(project_page));
                                     var link = $(lines[0]).find('a');
-                                    var status = $(lines[0]).find('img');
+                                    var status = $(lines[0]).find('img').attr('src');
                                     var plans = $(lines[5]).find('span');
                                     var banner = $(lines[13]).find('img');
                                     hyipResult.name = link.html();
                                     hyipResult.plans = plans.text().replace(/\s+/g,'|');
                                     hyipResult.banner_code = banner.attr('src');
-                                    hyipResult.status = status.attr('src');
-                                        request('http://monhyip.net'+link.attr('href'), function (error, response){
+                                    hyipResult.status = status;
+                                        request({uri: 'http://monhyip.net'+link.attr('href'), host: 'monhyip.net', path: link.attr('href')}, function (error, response){
                                             if (!error && response.statusCode == 200) {
                                                 hyipResult.URL = response.client._host;
                                                 /*params.save(hyipResult);
                                                 console.log(hyipResult);*/
+                                            }else{
+                                                /*console.log('ERROR from last response '+error);
+                                                console.log(hyipResult.id);*/
                                             }
                                         });
+                                }else{
+                                    /*console.log('ERROR from second response '+error);
+                                    console.log(hyipResult.id);*/
                                 }
                             });
                 });
+            }else{
+            /*console.log('ERROR from first response '+error);*/
             }
+
             }
         );
     }
